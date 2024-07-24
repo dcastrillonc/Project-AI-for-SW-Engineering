ff05fd41c5975877470a8b055ccce316dc7da2c1
j-arb <j.arboleda@uniandes.edu.co>
2024-07-24 17:43:37 -0500

    Add BetsPage

--- frontend/bets-smart/src/App.js
+++ frontend/bets-smart/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Login from './components/Login';
+import BetsPage from './pages/BetsPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Login/>
+      <BetsPage />
+      {/* <Login/> */}
       <Footer/>
     </div>
   );
---
+++ frontend/bets-smart/src/pages/BetsPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function BetsPage(props) {
+    return (
+        <div>
+            <h1>Your Bets</h1>
+            <h2>Win-Lose Bets</h2>
+            <h2>Result Bets</h2>
+            <p>You have no Result Bets</p>
+        </div>
+    )
+}
