2fe329a62dfce02cc3e51bef43b1d707c4912411
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2024-07-25 14:06:10 -0500

    feat: advances point 5

--- frontend/bets-smart/src/App.js
+++ frontend/bets-smart/src/App.js
@@ -1,22 +1,14 @@
-import { useEffect } from 'react';
+
 import './App.css';
 import EventsComponent from './components/EventsComponent';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Login from './components/Login';
-import BetsPage from './pages/BetsPage';
-import BetDetailPage from './pages/BetDetailPage';
-import BetsList from './components/bets-list/BetsList';
 import HomeComponent from './components/HomeComponent';
 import Profile from './components/Profile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Insights from './components/Insights';
 
 function App() {
-  useEffect(() => {
-    fetch("/api/v1/auth/dummy-login", {method: "post"}).then(res => {
-      res.json().then(console.log);
-    })
-  }, []);
   return (
     <div className="App">
       <Router>
@@ -25,8 +17,8 @@ function App() {
           <Route path="/" element={<HomeComponent />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/events" element={<EventsComponent />} />
-          {/* <Route path="/bets" element={<Bets />} />
-          <Route path="/insights" element={<Insights />} /> */}
+          {/* <Route path="/bets" element={<Bets />} />*/}
+          <Route path="/insights" element={<Insights />} /> 
         </Routes>
         <Footer />
       </Router>
